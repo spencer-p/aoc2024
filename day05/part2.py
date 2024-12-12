26fe885ae9a0d83315b24d484aefdb2bdeb76406
Spencer Peterson <spencerjpeterson@gmail.com>
2024-12-11 22:17:13 -0800

    day 5

---
+++ day05/part2.py
@@ -0,0 +1,41 @@
+import re
+
+f = open("input.txt", "r")
+rules, updates = f.read().strip().split("\n\n")
+rules = rules.split()
+updates = updates.split()
+
+def nums(s):
+    return list(map(int, re.findall(r'\d+', s)))
+
+graph = {}
+rgraph = {}
+for rule in rules:
+    a, b = nums(rule)
+    if a not in graph:
+        graph[a] = set()
+    graph[a].add(b)
+
+    if b not in rgraph:
+        rgraph[b] = set()
+    rgraph[b].add(a)
+
+
+def validate_update(rgraph, pages):
+    corrected = False
+    for i in range(len(pages)):
+        for j in range(i+1, len(pages)):
+            tocheck = rgraph.get(pages[i], None)
+            if tocheck is not None and pages[j] in tocheck:
+                pages[i], pages[j] = pages[j], pages[i]
+                corrected = True
+    return corrected
+
+total = 0
+for pageset in updates:
+    pages = nums(pageset)
+    if validate_update(rgraph, pages):
+        print(pages)
+        total += pages[len(pages)//2]
+
+print(total)
---
+++ day05/recap.md
@@ -0,0 +1,23 @@
+# mistakes
+
+- I had to write nums
+- I didn't use nums for one part and mixed chars and nums
+- i got bit by dictionaries not having good default/not found semantics
+- i forgot to force integer division
+- I range'd over the wrong numbers (values instead of index)
+- I wrote a bad if `i < j continue` line, when I meant the opposite That
+  wouldn't have been needed if I had gotten the range right the first time
+
+- i misread the problem for part 2 only asking for fixed lines
+
+# possible improvements
+
+- I wrote graph init, that could be a helper?
+- I am not used to ctrl-k to run yet
+- I could auto-copy the printed result
+- downloading the test input ?
+
+# wins
+
+- I got sub 30m
+- my input downloader worked well
